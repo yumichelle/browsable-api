ecbabd58cafd54fe720a439b41828c007da1028e
Michelle <y.michelle03@gmail.com>
2022-02-05 00:31:21 -0500

    need to figure out how to delete records. openfile function can get the csv and convert to df

--- UI/endpoint/views.py
+++ UI/endpoint/views.py
@@ -11,6 +11,7 @@ from .models import *
 from .serializers import *
 
 from modeldir.api import API
+
 # data_api = API(input, outout)
 
 class StudentList(generics.GenericAPIView):
@@ -22,16 +23,41 @@ class StudentList(generics.GenericAPIView):
         return Response(StudentList.queryset.values(), status=status.HTTP_200_OK)
 
     def put(self, request):
-        print(request.data) # <rest_framework.request.Request: PUT '/app/studentlist'>
         '''
+        (type(request.data))
+        <class 'django.http.request.QueryDict'>
+
+        (request) 
+        <rest_framework.request.Request: PUT '/app/studentlist'>
+
+        (request.data)
         <QueryDict: {'first_name': ['b'], 'last_name': ['m'], 
         'grade': ['A'], 'age': ['1'], 
         'photo': [<InMemoryUploadedFile: s-l640.jpg (image/jpeg)>]}>
+
+        dict(request.data)['photo'] or request.FILES['myfile']?
+        [<InMemoryUploadedFile: data.csv (application/vnd.ms-excel)>] 
+        InMemoryUploadedFile is a wrapper around a file object. 
+
+
+        (Student.objects.filter(pk=pk).values()) 
+        <QuerySet [{'id': 5, 'first_name': 'm', 'last_name': 'n', 'age': 3, 'grade': 'C', 'photo': 'uploads/data_ssqQyEb.csv'}]>
+   
+
+        file_in_memory = Student.objects.filter(pk=pk).values()
+        file_object = file_in_memory[0]['photo'] # gets csv
+        uploads/data_ssqQyEb.csv
+
         '''
         serializer = StudentSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
+            file = dict(request.data)['photo']
+            # open file to turn into df:
+            open_file(request.FILES['photo'])
+            # print(type(data), '\n', data)
+            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # way 2
 
@@ -41,15 +67,11 @@ class StudentDetail(APIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request):
-        return Response(StudentList.queryset.values(), status=status.HTTP_200_OK)
+    def get(self, request, pk, *args, **kwargs):
+        open_file(dict(request.data), pk)
+        return Response(Student.objects.filter(pk=pk).values(), status=status.HTTP_200_OK)
 
-    def put(self, request):
-        print(request)
-        if StudentDetail.is_valid(self):
-            StudentDetail.save()
-            return Response(StudentDetail.data, status=status.HTTP_201_CREATED)
-        return Response(StudentDetail.errors, status=status.HTTP_400_BAD_REQUEST) # way 2
+
 
 
 
@@ -69,3 +91,17 @@ def save_medical(request):
 @api_view(['GET'])
 def get_medical(request):
     return Response(Medical.objects.all().values(), status=status.HTTP_200_OK)
+
+
+import io
+import csv
+import pandas as pd
+def open_file(file, pk):
+
+    print (Student.objects.filter(pk=pk).values()) # <QuerySet [{'id': 5, 'first_name': 'm', 'last_name': 'n', 'age': 3, 'grade': 'C', 'photo': 'uploads/data_ssqQyEb.csv'}]>
+    file_in_memory = Student.objects.filter(pk=pk).values()
+    file_object = file_in_memory[0]['photo'] # gets csv
+    print(file_object)
+
+    df = pd.read_csv(file_object)
+    print (df)
