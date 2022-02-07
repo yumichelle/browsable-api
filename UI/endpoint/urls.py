732061b2b434eafeda7b5c1f10e5b99fc925dd57
Michelle <y.michelle03@gmail.com>
2022-02-06 22:24:35 -0500

    added swagger without openapi using drf-yasg

--- UI/endpoint/urls.py
+++ UI/endpoint/urls.py
@@ -1,12 +1,35 @@
 # <app>/urls.py
-from django.urls import path
+from django.urls import path, re_path
 
 from endpoint.views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="api.py API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@ "),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+# endpoints:
 urlpatterns = [
     # # path("<endpoint aka route>", views.<name of class>.as_view()),
     path('/studentlist', StudentList.as_view()),
     path('/<int:pk>', StudentDetail.as_view()),
     path('/save_medical', save_medical, name='save_contact'),
     path('/get_medical', get_medical, name='get_contact'),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # ^ A JSON view of your API specification at /swagger.json
+    # ^ A YAML view of your API specification at /swagger.yaml
+    path('/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
